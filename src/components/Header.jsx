a6adcb2b11e75e3d832d624b64e60ec4e55dfea6
Habib Fatkhul Rohman <95174265+habibfr@users.noreply.github.com>
2022-02-07 16:35:03 +0700

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import LunchDiningIcon from "@mui/icons-material/LunchDining";
+import HighlightIcon from "@mui/icons-material/Highlight";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <HighlightIcon fontSize="medium" />
+        Keeper Habib.
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
